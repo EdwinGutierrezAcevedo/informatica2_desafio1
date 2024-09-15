ebcf8582cd5969c3d1b1f6451def49e3f34ee237
EdwinGutierrezAcevedo <edwin.gutierrez1@udea.edu.co>
2024-09-15 15:31:18 -0500

    Creación de una función que muestre los resultados en la pantalla lcd.
    Signed-off-by: EdwinGutierrezAcevedo <edwin.gutierrez1@udea.edu.co>

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 int seconds = 0;
 int bottom = false;
 int analogPin = A0;//para ver grafica en el monitor serial
-int valor = 0; //para ver grafica en el monitor serial
-
-float val = 0;
+float valor = 0; //para ver grafica en el monitor serial
 float voltaje;
 float voltajeMax=0;
 float voltajeMin=0;
-
+float frecuencia;
+char tipo;
+int capacidad = 2;
+int* arr = new int[capacidad];
+int cantElementos=0;
 LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
+
 
 void setup()
 {
@@ -23,17 +26,18 @@ void setup()
 
 void loop()
 {
+
     if (digitalRead(13)==LOW) // si detecta boton inicio (izquierdo)
     {
         bottom = true;
-
+        voltajeMax=-10;
+        voltajeMin=10;
     }
     while( bottom == true) //mientras no se presione el boton final (derecho)
     {
         valor = analogRead(analogPin);
         voltaje=valor*(5/1023.0);
-        lcd_1.setCursor(0, 1);
-        Serial.println(voltaje);
+        Serial.println(voltaje); //graficar voltaje vs tiempo
         if (voltaje>=voltajeMax)    //guardar voltaje maximo
         {
             voltajeMax=voltaje;
@@ -42,16 +46,18 @@ void loop()
         {
             voltajeMin=voltaje;
         }
-
-        Serial.println(voltaje); //graficar voltaje vs tiempo
         if (digitalRead(8)==LOW)  // si detecta boton final
         {
-            bottom=false;          //cambiamos el valor para salir del ciclo
-
+            bottom=false;     //cambiamos el valor para salir del ciclo
+            mostrarResultados(voltajeMax, frecuencia,tipo);
         }
     }
+
+}
+
+void mostrarResultados(float voltaje,float frecuencia,char tipo){
     lcd_1.setCursor(0,0);
     lcd_1.print("Amplitud= ");
     lcd_1.setCursor(9,0);
-    lcd_1.print(voltajeMax);
+    lcd_1.print(voltaje);
 }
